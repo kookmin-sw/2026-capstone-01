cd7422d9dac26e2f5d522819f76d3f51095c8c18
Youngwook Lee <yamwook@naver.com>
2026-05-13 02:12:50 +0900

    mod: Onboarding flow 변경 및 MyPage UX 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter, Navigate, Route, Routes, useLocation, useNavigate,} from
 import type { NavigateFunction } from "react-router-dom";
 import AppShell from "./components/AppShell";
 import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
 import OnboardingPage from "./pages/OnboardingPage";
 import WithdrawalPendingPage from "./pages/WithdrawalPendingPage";
 import HomePage from "./features/tour/HomePage";
@@ -534,8 +533,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/register/onboarding" element={<OnboardingPage />} />
+          <Route path="/register" element={<OnboardingPage />} />
+          <Route path="/register/onboarding" element={<Navigate to="/register" replace />} />
           <Route path="/withdrawal-pending" element={<WithdrawalPendingPage />} />
           <Route element={<AppShell />}>
             <Route path="/home" element={<HomePage />} />
